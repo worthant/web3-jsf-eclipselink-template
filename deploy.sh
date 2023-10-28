799e852c166c5a54713c25a77377925ac69dd8d7
worthant <b_dvorkin@niuitmo.ru>
2023-10-29 01:16:34 +0300

    :wrench: Change to any isu id

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 echo "Deploying to Helios"
 
 ## Remove existing deployment
-ssh -p 2222 s368090@se.ifmo.ru "rm -rf wildfly-26.1.3/standalone/deployments/eclipselink-orm-jsf-demo-1.0-SNAPSHOT.war"
+ssh -p 2222 sXXXXXX@se.ifmo.ru "rm -rf wildfly-26.1.3/standalone/deployments/eclipselink-orm-jsf-demo-1.0-SNAPSHOT.war"
 # add new deployment
-scp -P 2222 ./target/eclipselink-orm-jsf-demo-1.0-SNAPSHOT.war s368090@se.ifmo.ru:wildfly-26.1.3/standalone/deployments
+scp -P 2222 ./target/eclipselink-orm-jsf-demo-1.0-SNAPSHOT.war sXXXXXX@se.ifmo.ru:wildfly-26.1.3/standalone/deployments
